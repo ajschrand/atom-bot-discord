59424dd5ba908530e7f801386f9e39304bda056d
ajschrand <58833566+ajschrand@users.noreply.github.com>
2020-11-30 14:07:24 -0500

    fix find_user_vp (again)

--- StudyTime.py
+++ StudyTime.py
@@ -63,7 +63,7 @@ async def on_message(message):
 
 async def find_user_vp(member: discord.Member, channel: discord.TextChannel):
     # find the last sent message from {member} in {channel} if one exists
-    messages = await channel.history(limit=10).flatten()  # get the last 10 messages in {channel} as :list:
+    messages = await channel.history().flatten()  # get the messages in {channel} as :list:
     for message in messages:
         if str(member.mention) in message.content:
             return message
